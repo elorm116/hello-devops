7ee5a2185ee16c0d3501f1b991207558af2ca652
Anthony Elorm Zottor <aezottor@gmail.com>
2026-02-03 16:37:04 +0300

    Initial commit: Basic Flask app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import platform
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return f"""
+    <h1>Hello DevOps!</h1>
+    <p>Server: {socket.gethostname()}</p>
+    <p>Platform: {platform.system()} {platform.release()}</p>
+    <p>This app is running in a container!</p>
+    """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
